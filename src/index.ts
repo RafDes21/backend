db415f54f4be12e9ceaf5bf6c7be61f0af08dd3f
unknown <israeltineo84@gmail.com>
2023-04-20 20:27:01 -0300

    [features] run build

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import "../envConfig";
 import express from "express";
 import cors from "cors";
 import route from "./routes/clients.routes";
+import bodyParser from 'body-parser';
 import dbConfig from "./bd/config";
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
