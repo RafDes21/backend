c31ce227d0abb604dc2edd2ee81cb580f6adaeea
unknown <israeltineo84@gmail.com>
2023-05-26 10:54:07 -0300

    add

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,9 @@ import bodyParser from "body-parser";
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
