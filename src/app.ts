7c0ea9565a2bd835553f85e1b768a14986c9dbbe
unknown <israeltineo84@gmail.com>
2023-05-26 11:31:00 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import bodyParser from "body-parser";
 const app = express();
 
 app.use(cors({
-    origin: '*'
+    origin: 'https://gestions-iota.vercel.app'
 }));
 app.use(bodyParser.json());
 app.use(express.json());
